9eda30f218e0c3b85f4aa7b8fbab374e5b7a1444
Bekir ÇELİK <126257699+bcelik32@users.noreply.github.com>
2024-01-09 23:45:58 +0300

    Add files via upload

---
+++ taskagitmakas/odalar.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace taskagitmakas
+{
+    partial class odalar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(odalar));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(279, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 53);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Oda Oluştur";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.tableLayoutPanel2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(652, 472);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aktif Odalar";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.83026F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74.16974F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 97F));
+            this.tableLayoutPanel2.Controls.Add(this.listBox3, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.listBox2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.listBox1, 1, 1);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(6, 45);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 383F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(640, 420);
+            this.tableLayoutPanel2.TabIndex = 11;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.listBox3.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 39;
+            this.listBox3.Location = new System.Drawing.Point(545, 40);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(92, 355);
+            this.listBox3.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 39;
+            this.listBox2.Location = new System.Drawing.Point(3, 40);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(134, 355);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(563, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 31);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Kişi";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(50, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 31);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(309, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "İsim";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 39;
+            this.listBox1.Location = new System.Drawing.Point(143, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(396, 355);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(6, 558);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(342, 32);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Yenile";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(15, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 33);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "Oda Adı";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold);
+            this.groupBox4.Location = new System.Drawing.Point(12, 490);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(503, 119);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Oda Oluştur";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(521, 508);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 101);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Ana Menü";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // odalar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(676, 621);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "odalar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "odalar";
+            this.Load += new System.EventHandler(this.odalar_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
---
+++ taskagitmakas/odalar.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+namespace taskagitmakas
+{
+    public partial class odalar : Form
+    {
+        public odalar()
+        {
+            InitializeComponent();
+        }
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            BasePath = "https://csharpfirebase-492fb-default-rtdb.europe-west1.firebasedatabase.app/",
+            AuthSecret = "y9KUFGNMTqX76Dky75qIpHmuNtpEAoZQrDk84axj"
+        };
+        IFirebaseClient client;
+
+
+
+        public class Data
+        {
+            public string secim1db { get; set; }
+            public string hazir1db { get; set; }
+            public string hazir2db { get; set; }
+            public string secim2db { get; set; }
+            public string biraktif { get; set; }
+            public string ikiaktif { get; set; }
+            public string odaid { get; set; }
+
+
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Oda Adı")
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Oda Adı";
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            Random random = new Random();
+            int randomNumber = random.Next(0, 10000);
+            // Eğer hata var ise null döner
+            if (client == null)
+            {
+                MessageBox.Show("Bağlantı hatası.");
+                return;
+            }
+
+            // "birinci" kümesinden mevcut veriyi çek
+            FirebaseResponse getResponseBirinci = await client.GetAsync(textBox1.Text+"/"+"birinci");
+            Data currentDataBirinci = getResponseBirinci.ResultAs<Data>();
+
+            // "ikinci" kümesinden mevcut veriyi çek
+            FirebaseResponse getResponseIkinci = await client.GetAsync(textBox1.Text + "/" + "ikinci");
+            Data currentDataIkinci = getResponseIkinci.ResultAs<Data>();
+
+            FirebaseResponse getResponseid = await client.GetAsync(textBox1.Text);
+            Data currentDataIid= getResponseIkinci.ResultAs<Data>();
+
+            // "birinci" kümesini güncelle ve sadece "hazir1db" ve "secim1db" alanlarını değiştir
+            var newDataBirinci = new Data
+            {
+                hazir1db = "false",
+                secim1db = "yok",
+                biraktif = "off",
+
+            };
+            FirebaseResponse updateResponseBirinci = await client.UpdateAsync(textBox1.Text + "/" + "birinci", newDataBirinci);
+
+            // "ikinci" kümesini güncelle ve sadece "hazir2db" ve "secim2db" alanlarını değiştir
+            var newDataIkinci = new Data
+            {
+                hazir2db = "false",
+                secim2db = "yok",
+                ikiaktif = "off"
+
+            };
+            FirebaseResponse updateResponseIkinci = await client.UpdateAsync(textBox1.Text + "/" + "ikinci", newDataIkinci);
+
+
+            var idyukle = new Data
+            {
+                
+                odaid = randomNumber.ToString()
+
+            };
+            FirebaseResponse updateResponseid= await client.UpdateAsync(textBox1.Text, idyukle);
+            yenile();
+            secilenoda = textBox1.Text;
+            player osece= new player();
+            Hide();
+            osece.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 anamenu = new Form2();
+            Hide();
+            anamenu.Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox1.Text=="Oda Adı")
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void odalar_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            yenile();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            yenile();
+        }
+       public static string secilenoda = "";
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            //label5.Text = listBox1.SelectedItem.ToString(); 
+            if (listBox1.SelectedItem.ToString() != "")
+            {
+                secilenoda = listBox1.SelectedItem.ToString();
+                player osece = new player();
+                Hide();
+                osece.Show();
+            }
+        }
+        private async void yenile()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            client = new FireSharp.FirebaseClient(config);
+
+            // Eğer hata var ise null döner
+            if (client == null)
+            {
+                MessageBox.Show("Bağlantı hatası.");
+                return;
+            }
+
+            // Firebase Realtime Database'den tüm çocuk düğümleri al
+            FirebaseResponse getResponse = await client.GetAsync("");
+
+            if (getResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Dictionary<string, object> data = getResponse.ResultAs<Dictionary<string, object>>();
+
+                // ListBox'ları temizle
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                //textBox1.Text = data.Keys.ToString();
+
+                
+                if (data != null)
+                {
+                    // Her bir oda adını ListBox'a ekleyin
+                    foreach (var roomName in data.Keys)
+                    {
+
+                        // Her bir oda adı için birinci ve ikinci içindeki değerleri çek
+                        FirebaseResponse birinciResponse = await client.GetAsync($"{roomName}/birinci");
+                        FirebaseResponse ikinciResponse = await client.GetAsync($"{roomName}/ikinci");
+                        FirebaseResponse fid = await client.GetAsync($"{roomName}");
+
+                        Data birinciData = birinciResponse.ResultAs<Data>();
+                        Data ikinciData = ikinciResponse.ResultAs<Data>();
+                        Data fiddata = fid.ResultAs<Data>();
+
+                        if (birinciData != null && ikinciData != null)
+                        {
+                            string birinciBiraktifValue = birinciData.biraktif;
+                            string ikinciIkiaktifValue = ikinciData.ikiaktif;
+                            string idstr = fiddata.odaid;
+                            listBox1.Items.Add(roomName);
+                            listBox2.Items.Add($"{idstr}");
+
+
+                            // Duruma göre listBox2'ye ekleme yap
+                            if (birinciBiraktifValue == "on" && ikinciIkiaktifValue == "on")
+                            {
+                                listBox3.Items.Add($"2/2");
+                            }
+                            else if (birinciBiraktifValue == "on" || ikinciIkiaktifValue == "on")
+                            {
+                                listBox3.Items.Add($"2/1");
+                            }
+                            else
+                            {
+                                listBox3.Items.Add($"2/0");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Oda Adı: {roomName} - Veri bulunamadı veya Oda yok.");
+                        }
+                    }
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Firebase'den veri alınamadı. Hata kodu: " + getResponse.StatusCode);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
